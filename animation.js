47ce748ed3faa985d93408ae69e1bcb7c3cbbcad
mattlourenco27 <matt.lourenco27@gmail.com>
2020-06-15 19:51:51 -0400

    Fixed bugs in animation where animation would play when scrolling up

--- animation.js
+++ animation.js
@@ -8,11 +8,9 @@ const slideOptions = {
 };
 const slideObserver = new IntersectionObserver(function(entries, slideObserver) {
 	entries.forEach(entry => {
-		if(entry.isIntersecting) {
+		if(entry.isIntersecting || entry.target.getBoundingClientRect().top < 0) {
 			entry.target.classList.add("appear");
 			slideObserver.unobserve(entry.target);
-		} else {
-			entry.target.classList.remove("appear");
 		}
 	});
 }, slideOptions);
