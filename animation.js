ee7bd3b1a999171697672feb29cc18c407e104d5
mattlourenco27 <matt.lourenco27@gmail.com>
2020-06-16 13:23:59 -0400

    Style and tansitions

--- animation.js
+++ animation.js
@@ -10,7 +10,8 @@ const slideObserver = new IntersectionObserver(function(entries, slideObserver) 
 	entries.forEach(entry => {
 		if(entry.isIntersecting || entry.target.getBoundingClientRect().top < 0) {
 			entry.target.classList.add("appear");
-			slideObserver.unobserve(entry.target);
+		} else if(!entry.isIntersecting && entry.target.getBoundingClientRect().top > 0) {
+			entry.target.classList.remove("appear");
 		}
 	});
 }, slideOptions);
